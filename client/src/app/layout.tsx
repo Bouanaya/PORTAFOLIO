0b3989b0dae6762fd7ee211bbd11595d3e34bf22
SoufineBouanaya <bouanayasoufiane@gmail.com>
2025-09-24 18:53:42 +0100

    feat(navbar): add semantic navigation with active link support and dark mode

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "../app/globals.css";
 import  Navbar  from "@/components/Navbar";
-import  Footer  from "@/components/Footer";
 import Sidebar  from "@/components/Sidebar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -14,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="flex-1 flex flex-col">
           <Navbar />
           <main className="flex-1 p-6 overflow-y-auto">{children}</main>
-          <Footer />
+       
         </div>
       </body>
     </html>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client';
+
+import Image from 'next/image';
+import { Button } from './ui/button';
+import NavLink from 'next/link'; // or your custom NavLink
+import { usePathname } from 'next/navigation';
+import modeIcon from '/public/mode.svg'
 
 export default function Navbar() {
-  return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  )
+  const MenuName = ['Accueil', 'A propos', 'Projets', 'Contact'];
+  const pathname = usePathname();
+
+  return ( 
+    <header className="w-full bg-white shadow-md px-8 py-2 flex justify-between items-center ">
+      <div className="flex gap-6">
+      {MenuName.map((name, i) => {
+        // convert name to URL path
+        let href = name.toLowerCase() === 'accueil' ? '/' : `/${name.toLowerCase().replace(/\s/g, '')}`;
+
+        // check if this link is active
+        const isActive = pathname === href;
+          // special case for Accueil
+  if (name.toLowerCase() === 'accueil') {
+    href = '/';
+  }
+
+        return (
+          <Button key={i} className={isActive ? 'bg-green-300' : ''} disabled ={isActive} >
+            <NavLink href={href} className="px-3 py-2 rounded-md ">
+              {name}
+            </NavLink>
+          </Button>
+        );
+      })}
+      
+      </div>
+      <div className="flex items-center">
+        <Button className='bg-gradient-to-r from-white via-black/20 to-black'>
+            <Image src={modeIcon} width={20} height={20} alt="Mode toggle" />
+        </Button>
+   
+      </div>
+    </header>
+  );
 }
